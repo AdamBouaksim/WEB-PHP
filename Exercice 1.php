5fcfff7c0c3a0da15ae7b255e9d9f6a01285348c
Adam Bouaksim <bouaksim-adam@lycee-jeanrostand.fr>
2023-09-29 16:42:10 +0200

    exercice moyenne/mediane

---
+++ Exercice 1.php
@@ -0,0 +1,12 @@
+<?php
+
+function triangles(){
+    for($i = 0; $i <= 10; $i++) {
+        for($j = 0; $j <= $i; $j++) {
+            echo "*";
+        }
+        echo "\n";
+    }
+}
+
+echo triangles();
